7070cdfe53dc2f223da35bf5da4437670923645b
JosephRidge <josephridge18@gmail.com>
2025-08-12 12:14:05 +0300

    update

---
+++ lib/models/car.dart
@@ -0,0 +1,11 @@
+class Car {
+  // attributes of my car
+  final String color;
+  final String shape;
+  final String brand;
+  final double size;
+
+  // constructor
+
+  Car(this.color, this.brand, this.shape, this.size);
+}
--- lib/pages/landing/mountsapp.dart
+++ lib/pages/landing/mountsapp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mountainapp/main.dart';
+import 'package:mountainapp/models/car.dart';
 
 class MountsApp extends StatelessWidget {
   const MountsApp({super.key});
@@ -24,7 +25,7 @@ class MountsApp extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [AppHeader()],
+        children: [AppHeader(), AppSearch()],
       ),
     );
   }
@@ -38,15 +39,84 @@ class AppHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(left: 30, right: 30, top: 30),
-      child: Column(
-        children: [ 
+      child: Row(
+        children: [
           ClipOval(
-            child: Image(
-              image: NetworkImage(
-                scale: 5,
-                "https://avatars.githubusercontent.com/u/5081804?v=4",
-              ),
+            child: Image.network(
+              "https://avatars.githubusercontent.com/u/5081804?v=4",
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
             ),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hello, Roman',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Good morning',
+                style: TextStyle(color: mainColor, fontSize: 12),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AppSearch extends StatelessWidget {
+  const AppSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Discover",
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25),
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 50,
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.search, color: Colors.grey),
+                      SizedBox(width: 10),
+                      Text('search', style: TextStyle(color: Colors.grey)),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: mainColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(Icons.tune, color: Colors.white),
+              ),
+            ],
           ),
         ],
       ),
