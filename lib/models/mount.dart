e4a2e47b4a12cbf17cf0b84970822679dfe54b73
JosephRidge <josephridge18@gmail.com>
2025-08-13 11:09:26 +0300

    update

---
+++ lib/models/mount.dart
@@ -0,0 +1,14 @@
+class MountModel {
+  // attributes of that Mountain
+  String path;
+  String name;
+  String location;
+  String description;
+
+  // constructor
+  MountModel({
+    this.path = '',
+     this.name = '', 
+     this.location = '',
+      this.description= ''});
+}
--- lib/pages/landing/mountsapp.dart
+++ lib/pages/landing/mountsapp.dart
@@ -1,6 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:mountainapp/main.dart';
+import 'package:mountainapp/models/app_bottom_bar_item.dart';
 import 'package:mountainapp/models/car.dart';
+import 'package:mountainapp/models/category.dart';
+import 'package:mountainapp/models/mount.dart';
+
+List<AppBottomBarItem> barItems = [
+  AppBottomBarItem(icon: Icons.home, label: 'Home', isSelected: true),
+  AppBottomBarItem(icon: Icons.explore, label: 'Explore', isSelected: false),
+  AppBottomBarItem(icon: Icons.turned_in_not, label: 'Tag', isSelected: false),
+  AppBottomBarItem(
+    icon: Icons.person_outline,
+    label: 'Profile',
+    isSelected: false,
+  ),
+];
+final List<MountModel> mountItems = [
+  MountModel(
+    path:
+        'https://sa.kapamilya.com/absnews/abscbnnews/media/2021/afp/01/17/20210116-mt-semeru-indonesia-ash-afp-s.jpg',
+    name: 'Mount Semeru',
+    description:
+        'Semeru, or Mount Semeru, is an active volcano in East Java, Indonesia. It is located in the subduction zone, where the Indo-Australia plate subducts under the Eurasia plate. It is the highest mountain on the island of Java.',
+    location: 'East Java',
+  ),
+  MountModel(
+    path:
+        'https://media-cdn.tripadvisor.com/media/photo-s/04/a5/6f/ce/dsc-5622jpg.jpg',
+    name: 'Mount Merbaru',
+    description:
+        'Mount Merbabu is a dormant stratovolcano in Central Java province on the Indonesian island of Java. The name Merbabu could be loosely translated as Mountain of Ash from the Javanese combined words; Meru means mountain and awu or abu means ash.',
+    location: 'Central Java',
+  ),
+  MountModel(
+    path:
+        'https://sa.kapamilya.com/absnews/abscbnnews/media/2021/afp/01/17/20210116-mt-semeru-indonesia-ash-afp-s.jpg',
+    name: 'Mauna Loa',
+    description:
+        'Mauna Loa is one of five volcanoes that form the Island of Hawaii in the U.S. state of Hawai in the Pacific Ocean. The largest subaerial volcano in both mass and volume, Mauna Loa has historically been considered the largest volcano on Earth, dwarfed only by Tamu Massif.',
+    location: 'Hawaii',
+  ),
+  MountModel(
+    path:
+        'https://cdn.images.express.co.uk/img/dynamic/78/590x/mount-vesuvius-1100807.jpg',
+    name: 'Mount Vesuvius',
+    description:
+        'Mount Vesuvius is a somma-stratovolcano located on the Gulf of Naples in Campania, Italy, about 9 km east of Naples and a short distance from the shore. It is one of several volcanoes which form the Campanian volcanic arc.',
+    location: 'Italy',
+  ),
+  MountModel(
+    path:
+        'https://sa.kapamilya.com/absnews/abscbnnews/media/2021/afp/01/17/20210116-mt-semeru-indonesia-ash-afp-s.jpg',
+    name: 'Mount Popocatépetl',
+    description:
+        'Popocatépetl is an active stratovolcano located in the states of Puebla, Morelos, and Mexico in central Mexico. It lies in the eastern half of the Trans-Mexican volcanic belt. At 5,426 m it is the second highest peak in Mexico, after Citlaltépetl at 5,636 m.',
+    location: 'Mexico',
+  ),
+];
+
+final List<CategoryModel> categories = [
+  CategoryModel(category: 'Mountain', icon: Icons.terrain),
+  CategoryModel(category: 'Forest', icon: Icons.park),
+  CategoryModel(category: 'Beach', icon: Icons.beach_access),
+  CategoryModel(category: 'Hiking', icon: Icons.directions_walk),
+];
 
 class MountsApp extends StatelessWidget {
   const MountsApp({super.key});
@@ -25,7 +88,13 @@ class MountsApp extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [AppHeader(), AppSearch()],
+        children: [
+          AppHeader(),
+          AppSearch(),
+          AppMountListView(),
+          AppCategoryList(),
+          AppBottomBar(),
+        ],
       ),
     );
   }
@@ -119,6 +188,192 @@ class AppSearch extends StatelessWidget {
             ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class AppMountListView extends StatelessWidget {
+  const AppMountListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: mountItems.length,
+        itemBuilder: (context, index) {
+          MountModel currentMount = mountItems[index];
+          return Container(
+            alignment: Alignment.bottomLeft,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(
+                image: NetworkImage(currentMount.path),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  currentMount.name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  currentMount.location,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class AppCategoryList extends StatelessWidget {
+  const AppCategoryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Category',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'See More',
+                  style: TextStyle(
+                    color: mainColor,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            margin: EdgeInsets.only(left: 10),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                CategoryModel currentCategory = categories[index];
+                return Container(
+                  width: 100,
+                  margin: EdgeInsets.only(top: 10, right: 10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey.withOpacity(0.2),
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(currentCategory.icon, color: mainColor),
+                      Text(
+                        currentCategory.category!,
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AppBottomBar extends StatefulWidget {
+  const AppBottomBar({super.key});
+
+  @override
+  State<AppBottomBar> createState() => _AppBottomBarState();
+}
+
+class _AppBottomBarState extends State<AppBottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: Offset.zero,
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: List.generate(barItems.length, (index) {
+          AppBottomBarItem currentBarItem = barItems[index];
+          Widget barItemWidget;
+
+          if (currentBarItem.isSelected) {
+            barItemWidget = Container(
+              padding: EdgeInsets.only(left: 15, top: 5, bottom: 5, right: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: mainColor,
+              ),
+              child: Row(
+                children: [
+                  Icon(currentBarItem.icon, color: Colors.white),
+                  SizedBox(width: 5),
+                  Text(
+                    currentBarItem.label,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            barItemWidget = IconButton(
+              onPressed: () {
+                setState(() {
+                  barItems.forEach((AppBottomBarItem item) {
+                    item.isSelected = item == currentBarItem;
+                  });
+                });
+              },
+              icon: Icon(currentBarItem.icon),
+              color: Colors.grey,
+            );
+          }
+          return barItemWidget;
+        }),
       ),
     );
   }
